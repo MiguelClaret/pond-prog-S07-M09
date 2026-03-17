3e923390e1af03b9cee03b460f9110a2e8bcc6ef
MiguelClaret <miguel.claret@sou.inteli.edu.br>
2026-03-17 11:06:30 -0300

    feat: implementa primeira parte da infra

---
+++ src/backend/src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  const swaggerConfig = new DocumentBuilder()
+    .setTitle('Backend API')
+    .setDescription('Documentacao da API do backend')
+    .setVersion('1.0.0')
+    .build();
+
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup('docs', app, swaggerDocument);
+
+  await app.listen(process.env.PORT ?? 3000);
+}
+bootstrap();
